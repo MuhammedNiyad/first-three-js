f80eab111fc895e7f07c05367516f47db2aa64ca
MohammedNiyad <niyadpunniyath007@gmail.com>
2025-09-10 08:29:20 +0530

    created a new three

---
+++ src/main.js
@@ -0,0 +1,58 @@
+import * as THREE from 'three';
+import './style.css'
+import { rotate } from 'three/tsl';
+import { OrbitControls } from 'three/examples/jsm/Addons.js';
+
+const w = window.innerWidth;
+const h = window.innerHeight;
+
+// declare renderer
+const renderer = new THREE.WebGLRenderer({antialias:true});
+renderer.setSize(w,h);
+document.body.appendChild(renderer.domElement);
+
+const fov = 75;
+const aspect = w/h;
+const near = 0.1;
+const far = 10;
+
+// declare camera
+const camera = new THREE.PerspectiveCamera(fov, aspect,near,far);
+camera.position.z = 2;
+
+// declare scene
+const scene = new THREE.Scene();
+
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
+controls.dampingFactor = 0.03;
+
+const geo = new THREE.IcosahedronGeometry(1.0, 30);
+const mat = new THREE.MeshStandardMaterial({
+  color:0xffffff,
+  flatShading:true,
+});
+
+const mesh = new THREE.Mesh(geo, mat);
+scene.add(mesh);
+
+const wireMat = new THREE.MeshBasicMaterial({
+  color:0xffffff,
+  wireframe:true,
+});
+
+const wireMesh = new THREE.Mesh(geo, wireMat);
+wireMesh.scale.setScalar(1.001)
+mesh.add(wireMesh);
+
+const hemiLight = new THREE.HemisphereLight(0x0099ff, 0xaa5500);
+scene.add(hemiLight);
+
+function animate(t){
+  requestAnimationFrame(animate);
+  mesh.rotation.y = t * 0.0001;
+  renderer.render(scene, camera);
+  controls.update();
+}
+
+animate()
